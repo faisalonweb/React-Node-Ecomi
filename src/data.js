fcf4a3ba3a8d03495b7138ed6d4efd6eab400fed
macbook <macbook@Macbooks-MacBook-Pro.local>
2021-03-30 20:03:58 +0500

    intial commit

---
+++ src/data.js
@@ -0,0 +1,78 @@
+const data = {
+    products : [
+        {
+            _id: '1',
+            name: 'Nike Slim Shirts',
+            category: 'Shirts',
+            images: '/images/p1.jpeg',
+            price: 120,
+            brand: 'Nike',
+            rating: '4.5',
+            numReviews: 10,
+            description: 'high quality product',
+
+        },
+        {
+            _id: '2',
+            name: 'Adidas Fit Shirts',
+            category: 'Shirts',
+            images: '/images/p2.jpeg',
+            price: 100,
+            brand: 'Adidas',
+            rating: '4.0',
+            numReviews: 10,
+            description: 'high quality product',
+
+        },
+        {
+            _id: '3',
+            name: 'Lacoste Free Shirts',
+            category: 'Shirts',
+            images: '/images/p3.jpeg',
+            price: 220,
+            brand: 'Lacoste',
+            rating: '4.5',
+            numReviews: 17,
+            description: 'high quality product',
+
+        },
+        {
+            _id: '4',
+            name: 'Nike Slim Pants',
+            category: 'Pantss',
+            images: '/images/p4.jpeg',
+            price: 20,
+            brand: 'Nike',
+            rating: '4.8',
+            numReviews: 100,
+            description: 'high quality product',
+
+        },
+        {
+            _id: '5',
+            name: 'Puma Slim Pants',
+            category: 'Pants',
+            images: '/images/p5.jpeg',
+            price: 320,
+            brand: 'Puma',
+            rating: '4.9',
+            numReviews: 110,
+            description: 'high quality product',
+
+        },
+        {
+            _id: '6',
+            name: 'Adidas Fit Pants',
+            category: 'Pants',
+            images: '/images/p6.jpeg',
+            price: 320,
+            brand: 'Adidas',
+            rating: '4.9',
+            numReviews: 140,
+            description: 'high quality product',
+
+        },
+    ],
+};
+
+export default data;
